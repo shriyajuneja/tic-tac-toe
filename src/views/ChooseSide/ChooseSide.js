e0903decf1ddf9011a7b7f923352d2c6d4f47d73
shriyajuneja <shriya.juneja20@gmai.com>
2020-04-26 08:47:25 +0530

    [fix] opacity of non selected side

--- src/views/ChooseSide/ChooseSide.js
+++ src/views/ChooseSide/ChooseSide.js
@@ -1,41 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChooseSide.css";
-import "../../assests/css/common.css"
+import "../../assests/css/common.css";
 
 function ChooseSide(props) {
+  const [side, setSide] = useState("cross");
   return (
     <div className="parent">
       <div className="flex align-center column container">
         <h3 className="mb-20">Pick your side</h3>
         <div className="flex mb-20">
-          <div className="mr-5 flex column align-center">
+          <div className={`mr-5 flex column align-center ${side==='zero' && 'transition-opacity'}`}>
             <img
               src={require("../../assests/images/x.png")}
               alt="cross"
               className="icons mb-12"
             />
             <label className="radio">
-              <input id="type" type="radio" name="type" />
+              <input
+                id="cross"
+                value="cross"
+                type="radio"
+                name="type"
+                checked={side === "cross"}
+                onChange={(e) => {
+                  setSide(e.target.value);
+                }}
+              />
               <span className="outer">
                 <span className="inner"></span>
               </span>
             </label>
           </div>
-          <div className="flex column align-center">
+          <div className={`mr-5 flex column align-center ${side==='cross' && 'transition-opacity'}`}>
             <img
               src={require("../../assests/images/o.png")}
               alt="zero"
               className="icons mb-12"
             />
             <label className="radio">
-              <input id="type" type="radio" name="type" />
+              <input
+                id="zero"
+                type="radio"
+                name="type"
+                value="zero"
+                checked={side === "zero"}
+                onChange={(e) => {
+                  setSide(e.target.value);
+                }}
+              />
               <span className="outer">
                 <span className="inner"></span>
               </span>
             </label>
           </div>
         </div>
-        <button className="white" onClick={()=>{props.history.push("/game")}}>Continue</button>
+        <button
+          className="white"
+          onClick={() => {
+            props.history.push("/game");
+          }}
+        >
+          Continue
+        </button>
       </div>
     </div>
   );
